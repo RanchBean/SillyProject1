f016160c1214ff64278fb5e337bc9aa0d8591eab
Ethan throndsen <Ethunderson1@gmail.com>
2015-12-15 13:43:15 -0700

    Added things need to finish color code

---
+++ SillyProject1/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  SillyProject1
+//
+//  Created by Throndsen, Ethan on 12/15/15.
+//  Copyright © 2015 CTEC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController
+{
+    @IBAction func redButton(sender: UIButton)
+    {
+        
+    }
+    @IBAction func blueButton(sender: UIButton)
+    {
+        
+    }
+    @IBAction func greenButton(sender: UIButton)
+    {
+        
+    }
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning()
+    {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
+
