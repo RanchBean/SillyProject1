6f396aa0048c6262de4fa4fa6c72d50f6b1f97f0
Ethan throndsen <Ethunderson1@gmail.com>
2015-12-17 12:38:44 -0700

    replaced buttons with sliders and switches to change buttons

--- SillyProject1/ViewController.swift
+++ SillyProject1/ViewController.swift
@@ -10,18 +10,30 @@ import UIKit
 
 class ViewController: UIViewController
 {
-    @IBAction func redButton(sender: UIButton)
+    @IBAction func changeRed(sender: UISlider)
     {
+     let newRed = CGFloat(Double(sender.value))
         
+     view.backgroundColor = UIColor(red: newRed, green: CGFloat(0.234), blue: CGFloat(0.459), alpha:1.0)
     }
-    @IBAction func blueButton(sender: UIButton)
+    @IBAction func changeGreen(sender: UISlider)
     {
-        
+        let newGreen = CGFloat(Double(sender.value))
+        view.backgroundColor = UIColor(red: CGFloat(0.123), green: newGreen, blue:CGFloat(0.234), alpha: 1.0)
     }
-    @IBAction func greenButton(sender: UIButton)
+    @IBAction func changeBlue(sender: UISwitch)
     {
-        
+        if(sender.on)
+        {
+            view.backgroundColor = UIColor(red:CGFloat(0.334), green: CGFloat(0.134), blue: CGFloat(0.999), alpha: 1.0)
+        }
+        else
+        {
+            view.backgroundColor = UIColor(red:CGFloat(0.334), green: CGFloat(0.134), blue: CGFloat(0.001), alpha: 1.0)
+        }
     }
+
+    
     override func viewDidLoad()
     {
         super.viewDidLoad()
@@ -33,12 +45,5 @@ class ViewController: UIViewController
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    
-    
-    
-    
-    
-    
-}
 
+}
